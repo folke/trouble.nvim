0fe9d7ad9d8e71cc685b397a4bf23b52fa2d083a
0x7a7a <298164449@qq.com>
2022-04-28 17:38:38 +0800

    fix: 'item.range' may be nil when exec 'workspace_diagnostics'

--- lua/trouble/util.lua
+++ lua/trouble/util.lua
@@ -123,12 +123,12 @@ function M.process_item(item, bufnr)
   local range = item.range or item.targetSelectionRange
 
   local start = {
-    line = vim.tbl_get(range, "start", "line") or item.col,
-    character = vim.tbl_get(range, "start", "character") or item.lnum,
+    line = range and vim.tbl_get(range, "start", "line") or item.col,
+    character = range and vim.tbl_get(range, "start", "character") or item.lnum,
   }
   local finish = {
-    line = vim.tbl_get(range, "end", "line") or item.end_col,
-    character = vim.tbl_get(range, "end", "character") or item.end_lnum,
+    line = range and vim.tbl_get(range, "end", "line") or item.end_col,
+    character = range and vim.tbl_get(range, "end", "character") or item.end_lnum,
   }
 
   if start.character == nil or start.line == nil then
