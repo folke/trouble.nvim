3773a901c27b99452088a06d9635af712b626727
Erik Høj Petersen <erik@setsnail.com>
2023-02-05 14:15:37 +0100

    Revert "first attempt at multithreading"
    This reverts commit 931ab009b98a7d50695613e2992a0ba2832bf740.

--- lua/trouble/providers/tsc.lua
+++ lua/trouble/providers/tsc.lua
@@ -5,6 +5,8 @@ local M = {}
 
 ---@param options TroubleOptions
 function M.tsc(_, buf, cb, options)
+  local items = {}
+
   local tsConfigPath = vim.fn.findfile("tsconfig.json", ".;")
 
   if tsConfigPath == "" then
@@ -14,31 +16,34 @@ function M.tsc(_, buf, cb, options)
 
   local command = "tsc --noEmit --pretty false --project " .. tsConfigPath .. " | tsc-output-parser"
 
-  local items = {}
-  vim.fn.jobstart(command, {
-	on_stdout = function(_, data, _)
-	  local errors = vim.json.decode(data[1])
-	  for _, error in ipairs(errors) do
-		local item = {
-		  bufnr = vim.fn.bufnr(error.value.path.value, true),
-		  -- filename = error.value.path.value,
-		  -- filepath = error.value.path.value,
-		  lnum = error.value.cursor.value.line,
-		  end_lnum = error.value.cursor.value.line,
-		  col = error.value.cursor.value.col,
-		  end_col = error.value.cursor.value.col,
-		  severity = 1,
-		  message = error.value.message.value,
-		  code = error.value.tsError.value.errorString,
-		}
+  local handle = io.popen(command)
+  if handle == nil then
+    print("Unable to start command: " .. command)
+    return
+  end
 
-		table.insert(items, util.process_item(item))
-	  end
-	end,
-	on_exit = function()
-	  cb(items)
-	end,
-  })
+  local result = handle:read("*a")
+  handle:close()
+
+  local errors = vim.json.decode(result)
+  for _, error in ipairs(errors) do
+    local item = {
+	  bufnr = vim.fn.bufnr(error.value.path.value, true),
+	  -- filename = error.value.path.value,
+	  -- filepath = error.value.path.value,
+      lnum = error.value.cursor.value.line,
+      end_lnum = error.value.cursor.value.line,
+      col = error.value.cursor.value.col,
+      end_col = error.value.cursor.value.col,
+      severity = 1,
+      message = error.value.message.value,
+      code = error.value.tsError.value.errorString,
+    }
+	
+    table.insert(items, util.process_item(item))
+  end
+
+  cb(items)
 end
 
 return M
