0afe9638f798d99f8a3c05c5b622fb0226ffe35d
Erik Høj Petersen <erik@setsnail.com>
2023-02-05 18:21:42 +0100

    wip

--- lua/trouble/providers/tsc.lua
+++ lua/trouble/providers/tsc.lua
@@ -3,21 +3,6 @@ local util = require("trouble.util")
 ---@class Lsp
 local M = {}
 
-function M.dump(o)
-  if type(o) == "table" then
-    local s = "{ "
-    for k, v in pairs(o) do
-      if type(k) ~= "number" then
-        k = '"' .. k .. '"'
-      end
-      s = s .. "[" .. k .. "] = " .. M.dump(v) .. ","
-    end
-    return s .. "} "
-  else
-    return tostring(o)
-  end
-end
-
 ---@param options TroubleOptions
 function M.tsc(_, buf, cb, options)
   local tsConfigPath = vim.fn.findfile("tsconfig.json", ".;")
