21f13b8595ff9bce9906b3a8dabf7e793aee42a9
Cormac Relf <web@cormacrelf.net>
2023-05-03 02:13:25 +1000

    Add config for cascading severity

--- lua/trouble/config.lua
+++ lua/trouble/config.lua
@@ -37,6 +37,10 @@ local defaults = {
     toggle_fold = { "zA", "za" }, -- toggle fold of current file
     previous = "k", -- preview item
     next = "j", -- next item
+    incr_min_severity = {"+", "="}, -- increase the minimum severity, or enables if disabled
+    decr_min_severity = {"-", "_"}, -- decrease the minimum severity, or disable if below Information
+    incr_cascading_severity = ")", -- increase the cascading severity threshold, enables if disabled
+    decr_cascading_severity = "(", -- decrease the cascading severity threshold, or disables
   },
   indent_lines = true, -- add an indent guide below the fold icons
   auto_open = false, -- automatically open the list when you have diagnostics
@@ -59,6 +63,14 @@ local defaults = {
     "lnum",
     "col",
   },
+  min_severity = nil, -- setting to "Information", "Warning", or "Error" will filter out any less severe LSP diagnostics
+  cascading_severity_threshold = nil, -- with this set, attempts to display one severity of LSP diagnostic at a time.
+                                      -- useful if you want to deal with errors (that stop your code compiling) before
+                                      -- dealing with warnings etc. set to "Hint" to simply enable; set to a higher
+                                      -- severity to display that and all lower severities in one layer.
+                                      -- for example, set to "Information" and once you get rid of errors, and then
+                                      -- get rid of warnings, you are shown info and hints.
+
 }
 
 ---@type TroubleOptions
@@ -67,6 +79,42 @@ M.options = {}
 
 function M.setup(options)
   M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
+  M.fix_severity(M.options)
+end
+
+-- this is a bit overboard to validate these settings, but fun
+local DiagnosticSeverity = vim.lsp.protocol.DiagnosticSeverity
+local to_severity = function(severity)
+  if not severity then return nil end
+  return type(severity) == 'string' and DiagnosticSeverity[severity] or severity
+end
+local severity_keys = vim.tbl_keys(DiagnosticSeverity)
+local severity_names = vim.tbl_filter(function(a) return type(a) == "string" end, severity_keys)
+table.sort(severity_names, function (a, b) return to_severity(a) < to_severity(b) end)
+local severity_names_joined = table.concat(severity_names, ", ")
+local severity_expected = "nil, number in range 1..=4, or {"..severity_names_joined .. "}"
+function sev_validate(s)
+  -- Diagnostics
+  return vim.tbl_contains(severity_keys, s)
+end
+function opt_sev_validate(s)
+  if s == nil then return true end
+  if type(s) == 'number' then return s end
+  return sev_validate(s)
+end
+
+function M.fix_severity(opts)
+  vim.validate {
+    min_severity = { opts.min_severity, opt_sev_validate, severity_expected },
+    cascading_severity = { opts.cascading_severity, opt_sev_validate, severity_expected },
+  }
+  -- make them 1..=4 or nil
+  opts.min_severity = to_severity(opts.min_severity)
+  -- min_severity being Hint just runs a no-op filter, so ignore it
+  if opts.min_severity == 4 then
+    opts.min_severity = nil
+  end
+  opts.cascading_severity = to_severity(opts.cascading_severity)
 end
 
 M.setup()
