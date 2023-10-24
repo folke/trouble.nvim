6f5c863955257debacf97eac7cf6b7e71dc69857
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-24 09:55:04 +0200

    fix(preview): better preview for multiline items

--- lua/trouble/view/preview.lua
+++ lua/trouble/view/preview.lua
@@ -2,7 +2,7 @@ local Render = require("trouble.view.render")
 local Util = require("trouble.util")
 
 local M = {}
-M.preview = nil ---@type {win:number,buf:number,view:table}?
+M.preview = nil ---@type {win:number,buf:number,view:table,cursor:number[]}?
 
 function M.close()
   local preview = M.preview
@@ -44,6 +44,7 @@ function M.open(view, item)
     win = main.win,
     buf = main.buf,
     view = vim.api.nvim_win_call(main.win, vim.fn.winsaveview),
+    cursor = vim.api.nvim_win_get_cursor(main.win),
   }
 
   -- prevent the buffer from being loaded with eventignore
@@ -51,6 +52,9 @@ function M.open(view, item)
   -- no autocmds should be triggered. So LSP's etc won't attach in the preview
   Util.noautocmd(function()
     if not vim.api.nvim_buf_is_loaded(item.buf) then
+      vim.bo[item.buf].swapfile = false
+      pcall(vim.fn.bufload, item.buf)
+      -- vim.api.nvim_del_autocmd(autocmd)
       vim.b[item.buf].trouble_preview = true
       local ft = vim.filetype.match({ buf = item.buf })
       if ft then
@@ -70,17 +74,19 @@ function M.open(view, item)
   if end_pos[1] == item.pos[1] and end_pos[2] == item.pos[2] then
     end_pos[2] = end_pos[2] + 1
   end
-  vim.api.nvim_buf_set_extmark(item.buf, Render.ns, item.pos[1] - 1, item.pos[2], {
-    end_row = end_pos[1] - 1,
-    end_col = end_pos[2],
-    hl_group = "TroublePreview",
-    strict = false,
-  })
+
   vim.api.nvim_buf_set_extmark(item.buf, Render.ns, item.pos[1] - 1, 0, {
     end_row = end_pos[1],
     -- end_col = end_pos[2],
     hl_group = "CursorLine",
     hl_eol = true,
+    strict = false,
+  })
+  -- only highlight the range if it's on the same line
+  vim.api.nvim_buf_set_extmark(item.buf, Render.ns, item.pos[1] - 1, item.pos[2], {
+    end_row = end_pos[1] - 1,
+    end_col = end_pos[2],
+    hl_group = "TroublePreview",
     strict = false,
   })
   return item
