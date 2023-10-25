cd50de9ee337fbe6483ceddfb21471435803eb80
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-25 22:45:12 +0200

    feat: added kind symbol highlights

--- lua/trouble/config/highlights.lua
+++ lua/trouble/config/highlights.lua
@@ -2,28 +2,62 @@ local Util = require("trouble.util")
 
 local M = {}
 
+-- stylua: ignore
 local defaults = {
   -- General
-  Normal = "NormalFloat",
-  Text = "Normal",
-  Preview = "Visual",
+  Normal            = "NormalFloat",
+  Text              = "Normal",
+  Preview           = "Visual",
+
   -- Item
-  FileName = "Directory",
-  Source = "Comment",
-  Pos = "LineNr",
-  Count = "TabLineSel",
+  FileName          = "Directory",
+  Directory         = "Directory",
+  IconDirectory     = "Special",
+  Source            = "Comment",
+  Pos               = "LineNr",
+  Count             = "TabLineSel",
+
   -- Indent Guides
-  Indent = "LineNr",
-  IndentFoldClosed = "CursorLineNr",
-  IndentFoldOpen = "TroubleIndent",
-  IndentTop = "TroubleIndent",
-  IndentMiddle = "TroubleIndent",
-  IndentLast = "TroubleIndent",
-  IndentWs = "TroubleIndent",
+  Indent            = "LineNr",
+  IndentFoldClosed  = "CursorLineNr",
+  IndentFoldOpen    = "TroubleIndent",
+  IndentTop         = "TroubleIndent",
+  IndentMiddle      = "TroubleIndent",
+  IndentLast        = "TroubleIndent",
+  IndentWs          = "TroubleIndent",
+
+  -- LSP Symbol Kinds
+  IconArray         = "@punctuation.bracket",
+  IconBoolean       = "@boolean",
+  IconClass         = "@type",
+  IconConstant      = "@constant",
+  IconConstructor   = "@constructor",
+  IconEnum          = "@lsp.type.enum",
+  IconEnumMember    = "@lsp.type.enumMember",
+  IconEvent         = "Special",
+  IconField         = "@field",
+  IconFile          = "Normal",
+  IconFunction      = "@function",
+  IconInterface     = "@lsp.type.interface",
+  IconKey           = "@lsp.type.keyword",
+  IconMethod        = "@method",
+  IconModule        = "@namespace",
+  IconNamespace     = "@namespace",
+  IconNull          = "@constant.builtin",
+  IconNumber        = "@number",
+  IconObject        = "@constant",
+  IconOperator      = "@operator",
+  IconPackage       = "@namespace",
+  IconProperty      = "@property",
+  IconString        = "@string",
+  IconStruct        = "@lsp.type.struct",
+  IconTypeParameter = "@lsp.type.typeParameter",
+  IconVariable      = "@variable",
 }
 
 function M.setup()
   M.link(defaults)
+  M.source("fs")
 end
 
 ---@param prefix? string
