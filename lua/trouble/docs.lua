15914f80e1fbfc67e184b5ae5cced526f4e306e2
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-03-27 17:47:54 +0100

    docs: docgen for API

--- lua/trouble/docs.lua
+++ lua/trouble/docs.lua
@@ -1,5 +1,6 @@
 local Config = require("trouble.config")
 local Docs = require("lazy.docs")
+local LazyUtil = require("lazy.util")
 
 local M = {}
 
@@ -14,6 +15,7 @@ function M.update()
       name = "Trouble",
     }),
     modes = M.modes(),
+    api = M.api(),
   })
 end
 
@@ -31,6 +33,47 @@ function M.modes()
   return { content = table.concat(lines, "\n") }
 end
 
+---@return ReadmeBlock
+function M.api()
+  local lines = vim.split(LazyUtil.read_file("lua/trouble/api.lua"), "\n")
+
+  local funcs = {}
+
+  ---@type string[]
+  local f = {}
+
+  for _, line in ipairs(lines) do
+    if line:match("^%-%-") then
+      f[#f + 1] = line
+    elseif line:match("^function") then
+      f[#f + 1] = line:gsub("^function M", [[require("trouble")]])
+      funcs[#funcs + 1] = table.concat(f, "\n")
+      f = {}
+    else
+      f = {}
+    end
+  end
+  local Actions = require("trouble.config.actions")
+  local names = vim.tbl_keys(Actions)
+  table.sort(names)
+
+  for _, k in ipairs(names) do
+    local desc = k:gsub("_", " ")
+    local action = Actions[k]
+    if type(Actions[k]) == "table" then
+      desc = action.desc or desc
+      action = action.action
+    end
+    if type(action) == "function" then
+      funcs[#funcs + 1] = ([[
+-- %s
+require("trouble").%s()]]):format(desc, k)
+    end
+  end
+  return { content = table.concat(funcs, "\n\n"), lang = "lua" }
+end
+
 M.update()
+-- M.api()
 
 return M
