9583cdc0a2f9610c5dfd33b2550d7591f24ba3fc
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-29 08:51:12 +0200

    test: use busted

---
+++ tests/busted.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/env -S nvim -l
+
+vim.env.LAZY_STDPATH = ".tests"
+load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
+
+-- Setup lazy.nvim
+require("lazy.minit").busted({
+  spec = {},
+})
