1a2efaf06d2966ffe3a1ef4a90d0bd8b9d870643
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-27 10:56:48 +0100

    fix(lsp): handle invalid line positions

--- lua/trouble/sources/lsp.lua
+++ lua/trouble/sources/lsp.lua
@@ -10,12 +10,15 @@ local Util = require("trouble.util")
 ---@param encoding string utf-8|utf-16|utf-32| defaults to utf-16
 ---@return integer byte (utf-8) index of `encoding` index `index` in `line`
 local function get_line_col(line, index, encoding)
-  if vim.str_byteindex then
-    -- FIXME: uses old-style func signature, since there's no way to
-    -- properly detect if new style is available
-    return vim.str_byteindex(line, index, encoding == "utf-16")
+  local function get()
+    if vim.str_byteindex then
+      -- FIXME: uses old-style func signature, since there's no way to
+      -- properly detect if new style is available
+      return vim.str_byteindex(line, index, encoding == "utf-16")
+    end
+    return vim.lsp.util._str_byteindex_enc(line, index, encoding)
   end
-  local ok, ret = pcall(vim.lsp.util._str_byteindex_enc, line, index, encoding)
+  local ok, ret = pcall(get)
   return ok and ret or #line
 end
 
